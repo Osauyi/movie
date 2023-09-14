358cda48cd3661ff6255d62c2eef2468732ad8b6
Osauyi <victorimafidon17@gmail.com>
2023-09-14 05:14:07 +0100

    change component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
-'use client' //
+'use client' // 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Image from 'next/image'
 import Home from "./movies"
