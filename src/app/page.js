b11c1ba953b104caa15d5821a251f07dcee40570
Osauyi <victorimafidon17@gmail.com>
2023-09-15 19:40:00 +0100

    change api link

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default function Home(){
   }
 };
 
-fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
   .then(response => response.json())
   .then((data) => {setDataList(data.results)} )
   .catch(err => console.error(err));
